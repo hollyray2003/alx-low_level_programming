c613d256c25bc7dbb5ab8bbf7a6042ce7865c341
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-16 19:44:28 -0700

    correction on warning sign 1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	printf("The size of char is : %d byte(s)\n", sizeof(char));
-	printf("The size of an int is: %d byte(s)\n",sizeof(int));
-	printf("The size of a long int is: %d byte(s)\n",sizeof(long int));
-	printf("The size of a float is: %d byte(s)\n",sizeof(float));
+	printf("Size of a char is : %d byte(s)\n", sizeof(char));
+	printf("Size of an int is: %d byte(s)\n",sizeof(int));
+	printf("Size of a long int is: %d byte(s)\n",sizeof(long int));
+	printf("Size of a float is: %d byte(s)\n",sizeof(float));
 	return (0);
 }
