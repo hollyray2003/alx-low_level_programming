f28ac9c84eead3a7f0184c300ea0251fb185a77a
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-16 18:45:23 -0700

    commit on displaying string with puts in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This is a programming
+ * by hollyray to print a string.
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
