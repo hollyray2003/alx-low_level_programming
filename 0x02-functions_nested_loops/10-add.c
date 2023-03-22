d9cee860ad7c570e6f7810663c66b7174308f4d1
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-22 00:45:39 -0700

    correction on betty style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * add - Function
- * 
- * @a and @b - the two variables
- * 
+ * @a and @b - the two variable
+ *
  * Return: sums up the two variables @a and @b
  */
 int add(int a, int b)
