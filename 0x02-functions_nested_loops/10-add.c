57310aebe829b8db5459b3be3b71b5772ba4a574
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-22 00:37:46 -0700

    addittion commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Function
+ * 
+ * @a and @b - the two variables
+ * 
+ * Return: sums up the two variables @a and @b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
