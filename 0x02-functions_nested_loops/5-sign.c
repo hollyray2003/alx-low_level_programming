f35a0daa8da479724500752f9fbdf9f53bbb37b2
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-21 23:22:17 -0700

    2nd correction on whitespace

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * @n: character to compare
  *
  * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero
- * 
+ *
  */
 
 int print_sign(int n)
