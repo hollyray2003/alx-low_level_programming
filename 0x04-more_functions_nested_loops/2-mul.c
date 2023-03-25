4380785c4e6c1b8fc2dcd0dae91df2a7ac71fe40
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-24 22:52:39 -0700

    multiplies two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - multiply function
+ * @a: first number to multiply
+ * @b: 2nd number to multiply
+ * Return: result of two
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,71 +15,9 @@ int _isdigit(int c);
  *  _isdigit - function checks to print digit
  *
  */
-void print_alphabet(void);
+int mul(int a, int b);
 /**
- * print_alphabet_x10 - prints alphabet 10 times
+ * mul(int a, int b) - Function multiplies two integers
  *
  */
-void print_alphabet_x10(void);
-/**
- * _islower - prints if c is lower case
- * @c: character to use
- * Return: 1 or 0
- */
-int _islower(int c);
-/**
- * _isalpha - prints if character is alphabet
- * @c: character to use
- * Return: 1 or 0
- */
-int _isalpha(int c);
-/**
- * print_sign - prints a sign based on n
- * @n: integer to input
- * Return: 1 or 0  or -1
- */
-int print_sign(int n);
-/**
- * _abs - gives absolute value
- * @int: integer
- * Return: integer or abs value of int
- */
-int _abs(int);
-/**
- * print_last_last - prints digit last
- *
- * @int: integer to input
- * Return: last digit of integer
- */
-int print_last_digit(int);
-/**
- * jack_bauer - jack bauer gooo
- *
- */
-void jack_bauer(void);
-/**
- * times_table - times this up
- *
- *
- */
-void times_table(void);
-/**
- * add - adds stuff up
- *
- * @int: integers to add
- * Return: returns a sum
- */
-int add(int, int);
-/**
- * print_to_98 - prints to 98
- * @n: integer to start at
- *
- */
-void print_to_98(int n);
-/**
- * print_times_table - whatever man
- *@n: integer to put in this table
- *
- */
-void print_times_table(int n);
 #endif
