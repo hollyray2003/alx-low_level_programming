01992a75586caa6dc5042eae7338587d93869aeb
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-03-27 15:13:36 -0700

    first commit on pointers, arrays and string

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme file is meant for 0x05-pointers_arrays_strings directory
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * This is a user defined header file
+ *
+ */
+void reset_to_98(int *n);
+/**
+ *
+ * reset_to_98 - function to resets to 98
+ *
+ */
+#endif
