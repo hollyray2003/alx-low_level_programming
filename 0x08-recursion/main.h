aa0d0d5075164cffcb450fbaba578b402280581b
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-04-05 14:31:43 -0700

    first recurssion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+/**
+ *
+ * This is my userdefined main header file
+ * This are my function definitions 
+ */
+void _puts_recursion(char *s);
+/**
+ * _puts_recurssion - this function puts
+ * a recurssion
+ * i had to break to be on a safe side
+ * @*s: The is an argument or member
+ *
+ */
+int _putchar(char c);
+#endif
