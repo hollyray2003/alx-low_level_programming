dd1319a7042d166bdcf8cfb2d23279836c10f4c3
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-04-13 16:19:17 -0700

    more correction

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,19 +1,6 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-/**
- * *string_nconcat - function to concat
- * enate strings
- * @*s1: Mmeber string 1
- * @*s2: member string 2
- * @n: number
- */
+#ifndef _MAIN_H_
+#define _MAIN_H_
 void *malloc_checked(unsigned int b);
-/**
- * malloc_checked - function tocheck malloc
- * @b: Mmeber int
- *
- */
-#endif
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+#endif
