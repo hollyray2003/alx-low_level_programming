7e39de13f3862edd64b5722d4e634102c5d30535
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-04-13 15:31:07 -0700

    malloc o task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 /**
- *
- * This is my user defined header file called main
- *
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-/**
  * *string_nconcat - function to concatenate strings
  * @*s1: Mmeber string 1
  * @*s2: member string 2
  * @n: number
  */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
 #endif
 
