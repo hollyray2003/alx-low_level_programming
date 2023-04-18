010a43fe1b9e787b15885b0a4ec46d4c95168204
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-04-18 05:12:53 -0700

    malloc 3

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -20,6 +20,7 @@ char *create_array(unsigned int size, char c);
  *
  */
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 char **strtow(char *str);
 /**
  * argstostr - function args to strings
