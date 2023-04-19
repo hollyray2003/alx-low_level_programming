dca694adc275c9df7da44c9d9cc671c29a889080
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-04-19 05:45:45 -0700

    commit on name print

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * print_name - Function to print name
+ * @*name: argument pointer for Name
+ * @*f: argument poiter f
+ * @*: argument pointer
+ * returns: return
+ *
+ */
+void print_name(char *name, void (*f)(char *));
+#endif
