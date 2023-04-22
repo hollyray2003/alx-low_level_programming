24fd22199c5869c611fced726644a8666b5e4004
Hollyray2003 <oladipupo.odedeyi@gmail.com>
2023-04-22 15:10:22 -0700

    added codeblock correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -23,7 +23,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(argSum, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
+	{
 		sum = sum + va_arg(argSum, int);
+	}
 	va_end(argSum);
 	return (sum);
 }
