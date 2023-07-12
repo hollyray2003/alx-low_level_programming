2f0b39b5887c67bcbc99e6cf3f6cbd2bf7fa527c
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-07-12 09:37:58 -0700

    commit on argc argv my name is

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This readme file is for Argc and Argv
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *
+ * This is a self defined header file
+ *
+ */
+int _putchar(char c);
+/**
+ * _putchar - Method or function
+ *  c@: member of the argument
+ *
+ */
+int _atoi(char *s);
+
+#endif
+
