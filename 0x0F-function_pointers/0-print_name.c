9a42b9b93ef9c9e16de10ff11772b4a93d407b73
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-07-19 08:18:01 -0700

    correction of first

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,13 @@
 #include "function_pointers.h"
 /**
  * print_name - function to prin name
- * @*name: argument name pointer
+ * @name: argument name pointer
  * @f: argument and pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+
 		f(name);
 }
 
