1893c3e5ae119669dbf02c72b403a6ce7c8cc1f9
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-08-01 05:21:17 -0700

    free up

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - This function frees up space
+ * @head: head of struct
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(head);
+		head = tmp->next;
+	}
+
+}
