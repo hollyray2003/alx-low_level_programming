4cedcea106447e42d93d8b1f04456c83ba1fae33
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-08-01 06:05:47 -0700

    more singly 4th correction on g

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 {
        	int number;
 	listint_t *temp;
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (0);
 	temp = *head;
 	*head = (*head)->next;
