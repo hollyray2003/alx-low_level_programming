f25d5508de2c55e98868a82e9ed173813c0b050c
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-08-01 05:39:48 -0700

    correction on more singly 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,8 +13,8 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		tmp = *head;
-		free(*head);
-		*head = tmp->next;
+		*head = (*head)->next;
+		free(tmp);
 	}
 	*head = NULL;
 }
