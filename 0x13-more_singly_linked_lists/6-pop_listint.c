033aed0d074e6b33c5a9e2c615c2fcf505e4d9cf
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-08-01 05:53:43 -0700

    more singly correction 3 on g

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,20 @@
 #include "lists.h"
+
 /**
  * pop_listint - pops out a node from list
  * @head: head address
  * Return: returns
  */
+
 int pop_listint(listint_t **head)
 {
-    int Number;
-    listint_t *temp;
-
-    if (head == NULL || *head == NULL)
-        return (0);
-
-    temp = *head;
-    *head = (*head)->next;
-    Number = temp->n;
-    free(temp);
-
-    return (Number);
+       	int number;
+	listint_t *temp;
+	if (head == NULL || *head == NULL)
+		return (0);
+	temp = *head;
+	*head = (*head)->next;
+	number = temp->n;
+	free(temp);
+	return (number);
 }
