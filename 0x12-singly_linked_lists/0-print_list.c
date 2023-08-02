0031a88cdef3fb863582f073dd4c88f0b2d6552a
Oladipupo Joshua Odedeyi <oladipupo.odedeyi@gmail.com>
2023-08-02 09:06:07 -0700

    correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-count++;
-}
-return (count);
+	int count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
